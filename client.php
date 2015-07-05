b9612ad513d964c3a0d132425e62348c74752e4f
edelangh <edelangh@student.42.fr>
2015-07-05 16:20:15 +0200

    Add border and protect against empty

--- client.php
+++ client.php
@@ -1,56 +1,60 @@
 <script src="js/select.js"></script>
 <body onload="init()">
 <?php
+// NOTE
+// Charger une 1er fois la list des produits pour eviter de la charger a chaque fois !
 
 $bdd = new PDO('mysql:host=localhost;dbname=serveur;charset=utf8', 'root', 'password');
 session_start();
 if (!isset($_SESSION['id']))
-	$_SESSION['id'] = 0;
+$_SESSION['id'] = 0;
 if (!isset($_SESSION['list']))
-	$_SESSION['list'] = Array();
+$_SESSION['list'] = Array();
 date_default_timezone_set('UTC');
 
 if (isset($_GET['type']))
 {
-	if ($_GET['type'] == "cancel")
-	{
-		if (isset($_SESSION['list'][$_GET['id']]))
-			if ($_SESSION['list'][$_GET['id']] > 0)
-			{
-				$_SESSION['list'][$_GET['id']] -= 1;
-				if ($_SESSION['list'][$_GET['id']] == 0)
-					unset($_SESSION['list'][$_GET['id']]);
-			}
-	}
-	else if ($_GET['type'] == "buy")
-	{
-		if (!isset($_SESSION['list'][$_GET['id']]))
-			$_SESSION['list'][$_GET['id']] = 0;
-		$_SESSION['list'][$_GET['id']] += 1;
-	}
-	else if ($_GET['type'] == "categorie")
-	{
-		$_SESSION['id'] = $_GET['id'];
-	}
-	else if ($_GET['type'] == "clear")
-	{
-		$_SESSION['list'] = Array();
-	}
-	else if ($_GET['type'] == "confirme")
-	{
-		header('Location: index.php?page=valide');
+  if ($_GET['type'] == "cancel")
+  {
+    if (isset($_SESSION['list'][$_GET['id']]))
+      if ($_SESSION['list'][$_GET['id']] > 0)
+      {
+        $_SESSION['list'][$_GET['id']] -= 1;
+        if ($_SESSION['list'][$_GET['id']] == 0)
+          unset($_SESSION['list'][$_GET['id']]);
+      }
+  }
+  else if ($_GET['type'] == "buy")
+  {
+    $res = $bdd->query('SELECT * FROM produits WHERE id='.$_GET['id'])->fetch();
+    if (!isset($_SESSION['list'][$_GET['id']]))
+      $_SESSION['list'][$_GET['id']] = 0;
+    if ($res['nbr'] > $_SESSION['list'][$_GET['id']])
+      $_SESSION['list'][$_GET['id']] += 1;
+  }
+  else if ($_GET['type'] == "categorie")
+  {
+    $_SESSION['id'] = $_GET['id'];
+  }
+  else if ($_GET['type'] == "clear")
+  {
+    $_SESSION['list'] = Array();
+  }
+  else if ($_GET['type'] == "confirme")
+  {
+    header('Location: index.php?page=valide');
 
-	/*
-		$reponse = $bdd->query('INSERT INTO ticket(list, prix_total, date, id_user) VALUES (' \
-	   .serialize($_SESSION['list']) \
-	   .(42) \
-	   .date("Y-m-d H:i:s") \
-	   .$_SESSION['id_use'].')');
-	 */
-		// Bon javoue que la requet est degueulasse !
-		// et qu'il faudrait utiliser prepare
-		// Mais bon il est 3H et il y en a qui bosse :D
-	}
+    /*
+       $reponse = $bdd->query('INSERT INTO ticket(list, prix_total, date, id_user) VALUES (' \
+       .serialize($_SESSION['list']) \
+       .(42) \
+       .date("Y-m-d H:i:s") \
+       .$_SESSION['id_use'].')');
+     */
+    // Bon javoue que la requet est degueulasse !
+    // et qu'il faudrait utiliser prepare
+    // Mais bon il est 3H et il y en a qui bosse :D
+  }
 }
 
 
@@ -63,19 +67,19 @@ $cat = $_SESSION['id'];
 <?php
 foreach ($_SESSION['list'] as $id => $value)
 {
-	try {
-		$reponse = $bdd->query('SELECT * FROM produits WHERE id='.$id);
-		while ($res = $reponse->fetch())
-		{
-			echo "<div class='produit' id=".$res['id'].">";
-			echo "<img src='".$res['img']."'></img>";
-			echo $value;
-			echo "</div>";
+  try {
+    $reponse = $bdd->query('SELECT * FROM produits WHERE id='.$id);
+    while ($res = $reponse->fetch())
+    {
+      echo "<div class='produit' id=".$res['id'].">";
+      echo "<img src='".$res['img']."'></img>";
+      echo $value;
+      echo "</div>";
 
-		}
-	} catch (Exception $e) {
-		echo "Et merde ...";
-	}
+    }
+  } catch (Exception $e) {
+    echo "Et merde ...";
+  }
 }
 ?>
 <div id="action">
@@ -89,9 +93,9 @@ foreach ($_SESSION['list'] as $id => $value)
 $reponse = $bdd->query('SELECT * FROM categorie');
 while ($res = $reponse->fetch())
 {
-	echo "<div>";
-	echo "<img class='categorie' id=".$res['id']." src='".$res['img']."'></img>";
-	echo "</div>";
+  echo "<div>";
+  echo "<img class='categorie' id=".$res['id']." src='".$res['img']."'></img>";
+  echo "</div>";
 }
 
 ?>
@@ -104,9 +108,13 @@ $reponse = $bdd->query('SELECT * FROM produits WHERE id_categorie='.$cat);
 echo "<div>";
 while ($res = $reponse->fetch())
 {
-	echo "<div class='produit' id=".$res['id'].">";
-	echo "<img src='".$res['img']."'></img>";
-	echo "</div>";
+  if ($res['nbr'] > 0 && $res['see'])
+    if ($res['nbr'] < $res['nbr_limit'])
+      echo "<div class='produit lowQuantity' id=".$res['id'].">";
+    else
+      echo "<div class='produit' id=".$res['id'].">";
+  echo "<img src='".$res['img']."'></img>";
+  echo "</div>";
 }
 echo "</div>";
 ?>
